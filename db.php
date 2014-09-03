cad2f1f0389772323a049f267d8840b73b7ddc2b
Ben Crisp <ben.crisp22@gmail.com>
2014-09-03 22:39:08 +0100

    Set up db configuration

---
+++ db.php
@@ -0,0 +1,18 @@
+<?php
+
+// Database details
+$db_name = 'helpdesk';
+$db_host = 'localhost';
+$db_user = 'helpdesk';
+$db_passwd = '1024768';
+
+// Connect to database
+$mysqli = new mysqli($db_host, $db_user, $db_passwd, $db_name);
+
+// Chek errors, print if any
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+?>
