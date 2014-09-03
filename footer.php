fbe7378d51b28abdc07a2a8d16aed623072eaa67
Ben Crisp <ben.crisp22@gmail.com>
2014-09-03 21:50:16 +0100

    Header and footer seperated from main page

---
+++ footer.php
@@ -0,0 +1,17 @@
+<footer>
+    <div class="row">
+        <div class="12-small columns">
+            <p class="footer">Copyright &copy; 2014 - Ben Crisp</p>
+        </div>
+    </div>
+</footer>
+<!-- End Page Content -->
+
+<!-- Call in jquery and js -->
+<script src="js/vendor/jquery.js"></script>
+<script src="js/foundation.min.js"></script>
+<script src="js/sticky_footer.js"></script>
+<script>
+    $(document).foundation();
+</script>
+</body>
---
+++ header.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>
+        <?php echo $pageName; ?>
+    </title>
+    <!-- Zurb Foundation main stylesheet -->
+    <link rel="stylesheet" href="css/foundation.css" />
+    <!-- Custom style sheet -->
+    <link rel="stylesheet" href="css/custom.css" />
+    <!-- Media queries style sheet -->
+    <link rel="stylesheet" href="css/media_queries.css" />
+    <!-- modernizr.js to detect browser CSS3 and HTML5 feature supported by users browser -->
+    <script src="js/vendor/modernizr.js"></script>
+    <!-- Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Bree+Serif' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
