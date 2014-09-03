5fd0516896fddd5def00a67ab9a4538ddbe41e03
Ben <ben.crisp22@gmail.com>
2014-09-03 22:40:25 +0100

    Fixed spelling/grammar in comments

--- db.php
+++ db.php
@@ -9,7 +9,7 @@ $db_passwd = '1024768';
 // Connect to database
 $mysqli = new mysqli($db_host, $db_user, $db_passwd, $db_name);
 
-// Chek errors, print if any
+// Check for errors, print if any
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
