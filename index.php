a78db34d0a4a034be957f6d1936f7bb874cea9be
Ben Crisp <ben.crisp22@gmail.com>
2014-09-03 21:50:47 +0100

    added footer, improved ui

--- index.php
+++ index.php
@@ -1,60 +1,38 @@
-<!doctype html>
-<html class="no-js" lang="en">
+<?php 
+$pageName="Helpdesk - Login"; 
+include 'header.php'; 
+?>
 
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Helpdesk</title>
-    <!-- Zurb Foundation main stylesheet -->
-    <link rel="stylesheet" href="css/foundation.css" />
-    <!-- Custom style sheet -->
-    <link rel="stylesheet" href="css/custom.css" />
-    <!-- modernizr.js to detect browser CSS3 and HTML5 feature supported by users browser -->
-    <script src="js/vendor/modernizr.js"></script>
-    <!-- Fonts -->
-    <link href='http://fonts.googleapis.com/css?family=Bree+Serif' rel='stylesheet' type='text/css'>
-</head>
+<!-- Begin Page Content -->
 
-<body>
-    
-    <!-- Begin Page Content -->
-
-    <div class="row">
-        <div id="loginContainer">
-            <!-- Left (top on small) - Headings etc -->
-            <div class="small-12 medium-7 large-7 columns login heading" id="">
-                <h1 class="logo">Helpdesk</h1>
-                <h4 class="subheader">Bishop Justus IT Support System</h1>
-            </div>
-            <!-- Right (bottom on small) - login form -->
-            <div class="small-12 medium-5 large-5 columns login" id="">
-                <form>
-                    <div class="row">
-                        <div class="small-12 medium-12 small-centered medium-uncentered columns">
-                            <input type="text" name="username" placeholder="Username" />
-                        </div>
+<div class="row">
+    <div id="loginContainer">
+        <!-- Left (top on small) - Headings etc -->
+        <div class="small-12 medium-7 large-7 columns login heading" id="">
+            <h1 class="logo">Helpdesk.</h1>
+            <h4 class="subheader">Bishop Justus IT Support System</h4>
+        </div>
+        <!-- Right (bottom on small) - login form -->
+        <div class="small-12 medium-5 large-5 columns login" id="">
+            <form>
+                <div class="row">
+                    <div class="small-12 medium-12 small-centered medium-uncentered columns">
+                        <input type="text" name="username" placeholder="Username" />
                     </div>
-                    <div class="row">
-                        <div class="small-12 medium-12 small-centered medium-uncentered columns">
-                            <input type="password" name="password" placeholder="Password" />
-                        </div>
+                </div>
+                <div class="row">
+                    <div class="small-12 medium-12 small-centered medium-uncentered columns">
+                        <input type="password" name="password" placeholder="Password" />
                     </div>
-                    <div class="row">
-                        <div class="small-12 medium-12 small-centered medium-uncentered columns">
-                            <a href="#" class="button postfix">Login</a>
-                        </div>
+                </div>
+                <div class="row">
+                    <div class="small-12 medium-12 small-centered medium-uncentered columns">
+                        <a href="#" class="button postfix">Login</a>
                     </div>
-                </form>
-            </div>
+                </div>
+            </form>
         </div>
     </div>
+</div>
 
-    <!-- End Page Content -->
-
-    <!-- Call in jquery and js -->
-    <script src="js/vendor/jquery.js"></script>
-    <script src="js/foundation.min.js"></script>
-    <script>
-        $(document).foundation();
-    </script>
-</body>
+<?php include 'footer.php';?>
--- user_home.php
+++ user_home.php
@@ -1,45 +1,34 @@
-<!doctype html>
-<html class="no-js" lang="en">
+<?php 
+$pageName="Helpdesk - Home"; 
+include 'header.php'; 
+?>
 
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Helpdesk</title>
-    <!-- Zurb Foundation main stylesheet -->
-    <link rel="stylesheet" href="css/foundation.css" />
-    <!-- Custom style sheet -->
-    <link rel="stylesheet" href="css/custom.css" />
-    <!-- modernizr.js to detect browser CSS3 and HTML5 feature supported by users browser -->
-    <script src="js/vendor/modernizr.js"></script>
-    <!-- Fonts -->
-    <link href='http://fonts.googleapis.com/css?family=Bree+Serif' rel='stylesheet' type='text/css'>
-</head>
-
-<body>
-
-    <!-- Begin Page Content -->
-    <!-- Logo and logout button -->
-    <div class="row">
-        <div class="small-12 large-10 columns">
-            <h1 class="logo heading">Helpdesk</h1>
-        </div>
-        <div class="small-12 large-2 columns">
-            <p class="logout"><a href="#">Logout</a></p>
-        </div>
+<!-- Begin Page Content -->
+<!-- Logo and logout + ticket history buttons -->
+<div class="row">
+    <div class="small-12 large-8 columns">
+        <h1 class="logo heading">Helpdesk.</h1>
+        <h4 class="subheader heading">Bishop Justus IT Support System</h4>
     </div>
-    <!-- Sub heading -->
-    <div class="row">
-        <div class="small-9 columns">
-            <h4 class="subheader heading">Bishop Justus IT Support System</h1>
-        </div>
+    <div class="small-12 large-4 columns">
+        <ul class="inline-list">
+            <li><a href="#" class="menu hide-for-small-only">Ticket History</a>
+            </li>
+            <li><a href="logout.php" class="menu hide-for-small-only">Logout</a>
+            </li>
+        </ul>
     </div>
+</div>
+<!-- Mobile menu -->
+<div class="row">
+    <div class="small-12 columns">
+        <p class="logout show-for-small-only"><a href="logout.php">Logout</a>
+        </p>
+        <p class="logout show-for-small-only"><a href="#">Ticket History</a>
+        </p>
+    </div>
+</div>
+<!-- Help Options -->
 
-    <!-- End Page Content -->
 
-    <!-- Call in jquery and js -->
-    <script src="js/vendor/jquery.js"></script>
-    <script src="js/foundation.min.js"></script>
-    <script>
-        $(document).foundation();
-    </script>
-</body>
+<?php include 'footer.php';?>
