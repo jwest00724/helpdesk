419f5217013eff0dca1472caf825b39509b479c1
Ben Crisp <ben.crisp22@gmail.com>
2014-09-04 12:16:20 +0100

    added session check to header

--- header.php
+++ header.php
@@ -1,3 +1,13 @@
+<?php
+//Start session
+session_start();
+
+//Check whether the session variable SESS_MEMBER_ID is present or not
+if(!isset($_SESSION['sess_user_id']) || (trim($_SESSION['sess_user_id']) == '')) {
+    header("location: index.php");
+    exit();
+}
+?>
 <!doctype html>
 <html class="no-js" lang="en">
 
