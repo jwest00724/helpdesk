3a7d07c80f2eae780543cf462e6b715a9ed6503c
Ben Crisp <ben.crisp22@gmail.com>
2014-09-05 22:39:28 +0100

    Added 30 min timeout

--- header.php
+++ header.php
@@ -1,12 +1,29 @@
 <?php
-//Start session
+// Start session
 session_start();
 
-//Check whether the session variable SESS_MEMBER_ID is present or not
+// Check whether the session variable SESS_MEMBER_ID is present or not / if user is logged in
 if(!isset($_SESSION['sess_user_id']) || (trim($_SESSION['sess_user_id']) == '')) {
     header("location: index.php");
     exit();
 }
+
+// set timeout period in seconds
+$inactive = 1800; // (30 mins)
+
+// check to see if $_SESSION['timeout'] is set
+if(isset($_SESSION['timeout']) ) {
+    $session_life = time() - $_SESSION['timeout'];
+    if($session_life > $inactive)
+    { 
+        // go to login page when idle
+        session_destroy(); 
+        header("Location: index.php?timeout");
+        exit();
+    }
+}
+$_SESSION['timeout'] = time();
+
 ?>
 <!doctype html>
 <html class="no-js" lang="en">
